e8c238bc45571ac1be2bc365bd3e7c51b35d83ac
Rob Peschke <rspesc31@gmail.com>
2020-04-16 19:07:39 -0400

    three renders of the list with title style working:

---
+++ src/components/ResultsList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ResultsList = ({ headerText }) => {
+  return (
+    <View>
+      <Text style={styles.titleStyle}>{headerText}</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  titleStyle: {
+    fontSize: 18,
+    fontWeight: "bold"
+  }
+});
+
+export default ResultsList;
--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -1,4 +1,28 @@
 import { useEffect, useState } from "react";
 import yelp from "../api/yelp";
 
-export default () => {};
+export default () => {
+  const [results, setResults] = useState([]);
+  const [errMsg, setErr] = useState("");
+
+  //call search api when component is first rendered
+  useEffect(() => {
+    searchApi("food");
+  }, []);
+
+  const searchApi = async searchInput => {
+    try {
+      const res = await yelp.get("/search", {
+        params: {
+          term: searchInput,
+          limit: 50,
+          location: "Harrisonburg, va"
+        }
+      });
+      setResults(res.data.businesses);
+    } catch (err) {
+      setErr("Something went wrong");
+    }
+  };
+  return [searchApi, results, errMsg];
+};
