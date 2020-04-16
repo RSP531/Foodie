e44b686a865e898eef32cb9efd6ef56b85c89e46
Rob Peschke <rspesc31@gmail.com>
2020-04-16 16:30:23 -0400

    finishing the main app stack navigator to help navigate between screens in the app

--- App.js
+++ App.js
@@ -2,6 +2,16 @@ import React from "react";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import SearchScreen from "./src/screens/SearchScreen";
 
-const navigator = createStackNavigator({
-  Search: SearchScreen
-});
+const navigator = createStackNavigator(
+  {
+    Search: SearchScreen
+  },
+  {
+    initialRouteName: "Search",
+    defaultNavigationOptions: {
+      title: "Business Search Page"
+    }
+  }
+);
+
+export default createAppContainer(navigator);
