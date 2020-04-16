f834e3ff2ea20dbd97b05ad6bd305014d5c79fbd
Rob Peschke <rspesc31@gmail.com>
2020-04-16 17:43:23 -0400

    adding initial search

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import yelp from "../api/yelp";
@@ -8,11 +8,15 @@ const SearchScreen = () => {
   const [results, setResults] = useState([]);
   const [err, setErr] = useState("");
 
-  const searchApi = async () => {
+  useEffect(() => {
+    searchApi("food");
+  });
+
+  const searchApi = async searchInput => {
     try {
       const res = await yelp.get("/search", {
         params: {
-          input,
+          searchInput,
           limit: 50,
           location: "Harrisonburg, va"
         }
@@ -28,9 +32,9 @@ const SearchScreen = () => {
       <SearchBar
         input={input}
         onInputChange={setInput}
-        onTermSubmit={searchApi}
+        onTermSubmit={() => searchApi(input)}
       />
-      <Text>{err}</Text>
+      {err ? <Text>{err}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
   );
